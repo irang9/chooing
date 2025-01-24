c7c0f276f50693c0837f7fb60f40fad993b75c9a
irang <irang.ghang@gmail.com>
2025-01-24 15:00:36 +0900

    지긋지긋한 마이그레이션 파일

--- database/migrations/2025_01_22_163156_add_company_phone_and_extension_to_staff_table.php
+++ database/migrations/2025_01_22_163156_add_company_phone_and_extension_to_staff_table.php
@@ -4,15 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTypeToVacationsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('vacations', function (Blueprint $table) {
-            $table->string('type')->default('연차')->change();
+        Schema::table('staff', function (Blueprint $table) {
+            //
         });
     }
 
@@ -21,8 +21,8 @@ class AddTypeToVacationsTable extends Migration
      */
     public function down(): void
     {
-        Schema::table('vacations', function (Blueprint $table) {
-            $table->string('type')->default(null)->change();
+        Schema::table('staff', function (Blueprint $table) {
+            //
         });
     }
-}
+};
